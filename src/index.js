d398271b47afa780a39c3f08c66b9239c8e57962
OhET3388 <eeteik.oh@gmail.com>
2025-08-19 23:50:59 +0800

    11:50PM 19-AUG-2025

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Dockerized App v2!***");
+  res.send("Hello from Dockerized App v2!****");
 });
 
 const port = process.env.PORT || 3000;
